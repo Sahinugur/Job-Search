12426752fb926dbd4d9a2c4d98ac1bddc531cd96
Sahinugur <ugurlao@gmail.com>
2023-04-12 22:46:46 +0200

    installation of expo

---
+++ app/index.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter} from 'expo-router'
+
+import {COLORS, icons, images, SIZES} from '../constants'
+import {NearbyJobCard, Popularjobs, ScreenHeaderBtn, Welcome} from '../components'
+
+
+
+const Home = () =>{
+    const router = useRouter()
+    return(
+        <SafeAreaView style={{flex:1, backgroundColor: COLORS.lightWhite}}>
+         <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />
+          ),
+          headerTitle: "",
+        }}
+      />
+        </SafeAreaView>
+    )
+}
+export default Home
